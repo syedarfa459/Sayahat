b0ea9764c8034dae0cdcbf9bbb34682c9cd2efe7
Syed Muhammad Arfa <73629315+syedarfa459@users.noreply.github.com>
2021-06-12 14:19:09 +0500

    Update settings.py

--- Sayahat/settings.py
+++ Sayahat/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sayahatpk@gmail.com'
-EMAIL_HOST_PASSWORD = 'nqmvadpqhlodfpxd'
+EMAIL_HOST_USER = 'username'
+EMAIL_HOST_PASSWORD = 'password'
 # DEFAULT_FROM_EMAIL = 'arfashahji@gmail.com'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
